1b3d4297eb4bb5836a929c2f42914649f96a757a
Rishi <gargrishi02@gmail.com>
2025-04-06 00:27:03 -0400

    Initial Commit

---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Rishi Garg on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
